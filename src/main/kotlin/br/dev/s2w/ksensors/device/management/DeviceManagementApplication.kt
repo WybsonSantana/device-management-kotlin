caede37b28fe21198655981cd91242fc1f0f51b5
WybsonSantana <wybson@hotmail.com>
2025-04-05 18:39:52 -0300

    KSensors: #01 - 08.08 - Multiprojects with IntelliJ IDEA

---
+++ src/main/kotlin/br/dev/s2w/ksensors/device/management/DeviceManagementApplication.kt
@@ -0,0 +1,11 @@
+package br.dev.s2w.ksensors.device.management
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DeviceManagementApplication
+
+fun main(args: Array<String>) {
+	runApplication<DeviceManagementApplication>(*args)
+}

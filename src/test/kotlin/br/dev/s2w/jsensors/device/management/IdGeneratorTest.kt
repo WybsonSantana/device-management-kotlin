ef2d5e2c45f57153d82f8470d246a58e472d8400
WybsonSantana <wybson@hotmail.com>
2025-04-13 17:11:39 -0300

    KSensors: #01 - 09.05 - Using TSID with Kotlin

---
+++ src/test/kotlin/br/dev/s2w/jsensors/device/management/IdGeneratorTest.kt
@@ -0,0 +1,18 @@
+package br.dev.s2w.jsensors.device.management
+
+import org.assertj.core.api.Assertions
+import org.junit.jupiter.api.Test
+import java.time.Instant
+import java.time.temporal.ChronoUnit
+
+internal class IdGeneratorTest {
+
+    @Test
+    fun `should generate tsid`() {
+        val tsid = IdGenerator.generateTSID()
+
+        Assertions.assertThat(tsid.instant)
+            .isCloseTo(Instant.now(), Assertions.within(1, ChronoUnit.MINUTES))
+    }
+
+}

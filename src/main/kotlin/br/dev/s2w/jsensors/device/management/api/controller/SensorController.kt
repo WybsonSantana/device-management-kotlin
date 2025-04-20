984904270c2b9b9930dc452887ea2f39677b3645
WybsonSantana <wybson@hotmail.com>
2025-04-20 00:52:12 -0300

    KSensors: #01 - 10.06 - Implementing sensor query

--- src/main/kotlin/br/dev/s2w/jsensors/device/management/api/controller/SensorController.kt
+++ src/main/kotlin/br/dev/s2w/jsensors/device/management/api/controller/SensorController.kt
@@ -7,14 +7,22 @@ import br.dev.s2w.jsensors.device.management.common.IdGenerator
 import br.dev.s2w.jsensors.device.management.domain.model.Sensor
 import br.dev.s2w.jsensors.device.management.domain.model.SensorId
 import br.dev.s2w.jsensors.device.management.domain.repository.SensorRepository
+import io.hypersistence.tsid.TSID
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.*
+import org.springframework.web.server.ResponseStatusException
 
 @RestController
 @RequestMapping("/api/sensors")
 class SensorController(
     private val sensorRepository: SensorRepository
 ) {
+
+    @GetMapping("/{sensorId}")
+    fun getOne(@PathVariable sensorId: TSID): SensorOutput =
+        sensorRepository.findById(SensorId(sensorId))
+            .orElseThrow { ResponseStatusException(HttpStatus.NOT_FOUND) }
+            .toSensorOutput()
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)

8c8c4e454e521cff50004ee3f17abfbd92f47fb5
WybsonSantana <wybson@hotmail.com>
2026-07-26 17:26:46 -0300

    KSensors: #01 - 08.07 - Creating microservices with Spring Boot and Gradle

---
+++ src/test/kotlin/br/dev/s2w/ksensors/device/management/DeviceManagementApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.dev.s2w.ksensors.device.management
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DeviceManagementApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}

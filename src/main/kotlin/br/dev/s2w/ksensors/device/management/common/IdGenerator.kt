e4e003266c45036a25123742f8c7d7f98b610f66
WybsonSantana <wybson@hotmail.com>
2026-09-13 15:12:36 -0300

    KSensors: #01 - 09.05 - Using TSID with Kotlin

---
+++ src/main/kotlin/br/dev/s2w/ksensors/device/management/common/IdGenerator.kt
@@ -0,0 +1,25 @@
+package br.dev.s2w.ksensors.device.management.common
+
+import io.hypersistence.tsid.TSID
+
+class IdGenerator {
+
+    companion object {
+        private var tsidFactory: TSID.Factory
+
+        init {
+            System.getenv("tsid.node")?.let {
+                System.setProperty("tsid.node", it)
+            }
+
+            System.getenv("tsid.node.count")?.let {
+                System.setProperty("tsid.node.count", it)
+            }
+
+            tsidFactory = TSID.Factory.builder().build()
+        }
+
+        fun generateTSID(): TSID = tsidFactory.generate()
+    }
+
+}

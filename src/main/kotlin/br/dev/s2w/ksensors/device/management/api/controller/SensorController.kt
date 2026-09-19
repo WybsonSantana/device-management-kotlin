7ea909fcbadcaa5ff8320f389af8a2ebb953d786
WybsonSantana <wybson@hotmail.com>
2026-09-19 19:06:53 -0300

    KSensors: #01 - 10.04 - Implementing persistence with Jakarta Persistence and Spring Data JPA

--- src/main/kotlin/br/dev/s2w/ksensors/device/management/api/controller/SensorController.kt
+++ src/main/kotlin/br/dev/s2w/ksensors/device/management/api/controller/SensorController.kt
@@ -2,26 +2,30 @@ package br.dev.s2w.ksensors.device.management.api.controller
 
 import br.dev.s2w.ksensors.device.management.api.model.SensorInput
 import br.dev.s2w.ksensors.device.management.common.IdGenerator
-import br.dev.s2w.ksensors.device.management.model.Sensor
+import br.dev.s2w.ksensors.device.management.domain.model.Sensor
+import br.dev.s2w.ksensors.device.management.domain.model.SensorId
+import br.dev.s2w.ksensors.device.management.domain.repository.SensorRepository
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.*
 import org.springframework.web.bind.annotation.RequestMapping
 
 @RestController
 @RequestMapping("/api/sensors")
-class SensorController {
+class SensorController(
+    private val sensorRepository: SensorRepository
+) {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     fun create(@RequestBody input: SensorInput): Sensor =
         Sensor(
-            id = IdGenerator.generateTSID(),
+            id = SensorId(IdGenerator.generateTSID()),
             name = input.name,
             ip = input.ip,
             location = input.location,
             protocol = input.protocol,
             model = input.model,
             enabled = false
-        )
+        ).let(sensorRepository::saveAndFlush)
 
 }
